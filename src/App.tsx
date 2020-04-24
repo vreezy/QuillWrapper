b319ad01d9ecd1758d6d8c28bb0310e95dd8a4d1
vreezy117 <vreezy117@googlemail.com>
2020-04-24 23:57:01 +0200

    added description

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import React, { useState } from 'react';
 import React from 'react';
 
-import { TextField } from 'office-ui-fabric-react/lib/TextField';
+//import { TextField } from 'office-ui-fabric-react/lib/TextField';
 // import { Icon } from '@fluentui/react/lib/Icon';
 
 
@@ -16,18 +16,19 @@ function App() {
     <div className="container">
       <div className="row">
         <div className="column col">
-          <h1>Hallo welt</h1>
-          React.ts - react-quill - Fluent UI
+          <h3>Hallo Welt</h3>
+          <p>
+            Technologies: React.ts - react-quill - Fluent UI<br/>
+            Target: Style react-quill like Fluent UI Component<br/>
+            &nbsp; -> https://developer.microsoft.com/de-DE/fluentui#/<br/>
+            Download Code on GIT<br/>
+            &nbsp; -> https://github.com/vreezy/QuillWrapper<br/>
+          </p> 
+          <br />
           
           <Quillwrapper />
           
           <br />
-
-          <TextField />
-
-          <br />
-
-          <TextField />
         </div>
       </div>
     </div>
